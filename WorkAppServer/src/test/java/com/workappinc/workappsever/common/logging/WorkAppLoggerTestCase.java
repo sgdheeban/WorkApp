faff99f693655957562ae9e355ba1ce029bcb7b2
sgdheeban <sgdheeban@gmail.com>
2015-11-16 22:04:34 -0800

    @Ignore for unimplemented testcase

--- WorkAppServer/src/test/java/com/workappinc/workappsever/common/logging/WorkAppLoggerTestCase.java
+++ WorkAppServer/src/test/java/com/workappinc/workappsever/common/logging/WorkAppLoggerTestCase.java
@@ -2,11 +2,12 @@ package com.workappinc.workappsever.common.logging;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class WorkAppLoggerTestCase {
 
-	@Test
+	@Test @Ignore
 	public void test() {
 		fail("Not yet implemented");
 	}
