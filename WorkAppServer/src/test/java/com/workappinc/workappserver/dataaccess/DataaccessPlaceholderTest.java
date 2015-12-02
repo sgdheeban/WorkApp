a6de81e967d219bd669c38cdc8919675cd19b128
sgdheeban <sgdheeban@gmail.com>
2015-12-01 19:31:11 -0800

    Adding Command Line and Properties Args Reader

--- WorkAppServer/src/test/java/com/workappinc/workappserver/dataaccess/DataaccessPlaceholderTest.java
+++
@@ -1,6 +0,0 @@
-package com.workappinc.workappserver.dataaccess;
-
-public class DataaccessPlaceholderTest
-{
-
-}
---
+++ WorkAppServer/src/test/java/com/workappinc/workappserver/dataaccess/WorkAppArgReaderEnumTest.java
@@ -0,0 +1,41 @@
+package com.workappinc.workappserver.dataaccess;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.workappinc.workappserver.dataaccess.resources.WorkAppArgument;
+import com.workappinc.workappserver.dataaccess.resources.WorkAppCommandLineArgsReader;
+
+/**
+ * Test Suits for WorkAppArgument Reading from Command Line and Properties - using Enum
+ * @author dhgovindaraj
+ *
+ */
+public class WorkAppArgReaderEnumTest {
+	
+	/**
+	 * Testing if Enum Values can be used
+	 */
+	@Test
+	public void testCanUseAnEnumValue() {
+		CommandCLI cli = new CommandCLI();
+		String[] args = new String[] {"-command", "START"};
+		WorkAppCommandLineArgsReader.parse(cli, args);
+		
+		assertNotNull("Commands enum value not built", cli.getCommand());
+		assertEquals("retrieved command value is not Commands.START", Commands.START, cli.getCommand());
+	}
+	
+	public static class CommandCLI {
+		@WorkAppArgument()
+		private Commands command;
+
+		public Commands getCommand() {
+			return command;
+		}
+	}
+	public static enum Commands {
+		START, STOP, PAUSE;
+	}
+}
