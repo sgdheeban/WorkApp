0f7dc41414ed70efe64edd08da0c0c54759e0bce
sgdheeban <sgdheeban@gmail.com>
2015-12-08 13:24:51 -0800

    adding IEntry and IDataManager interfaces

---
+++ WorkAppServer/src/main/java/com/workappinc/workappserver/dataaccess/entry/IEntry.java
@@ -0,0 +1,11 @@
+package com.workappinc.workappserver.dataaccess.entry;
+
+/**
+ * IEntry is a generic database entry interface
+ * @author dhgovindaraj
+ *
+ */
+public interface IEntry
+{
+
+}
---
+++ WorkAppServer/src/main/java/com/workappinc/workappserver/dataaccess/resources/interfaces/IDataManager.java
@@ -0,0 +1,11 @@
+package com.workappinc.workappserver.dataaccess.resources.interfaces;
+
+/**
+ * IDataManager is a generic interface which queues IEntry objects for a periodic database update
+ * @author dhgovindaraj
+ *
+ */
+public interface IDataManager
+{
+
+}
