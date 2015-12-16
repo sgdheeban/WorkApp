6f92c4200abdd91ebdedfdf7a4adc62379e267bb
sgdheeban <sgdheeban@gmail.com>
2015-12-16 00:46:50 -0800

    Profile Page additions

--- WorkAppServer/src/main/java/com/workappinc/workappserver/businesslogic/pages/CommonBannerPage.java
+++
@@ -1,6 +0,0 @@
-package com.workappinc.workappserver.businesslogic.pages;
-
-public class CommonBannerPage implements IPage
-{
-
-}
--- WorkAppServer/src/main/java/com/workappinc/workappserver/businesslogic/pages/WorkAppUserProfilePage.java
+++ WorkAppServer/src/main/java/com/workappinc/workappserver/businesslogic/pages/WorkAppUserProfilePage.java
@@ -3,53 +3,117 @@ package com.workappinc.workappserver.businesslogic.pages;
 /**
  * WorkAppUserProfilePage is an instance of IPage interface, which models the
  * user through a digital profile
- * 
  * @author dhgovindaraj
  *
  */
 public class WorkAppUserProfilePage implements IPage
 {
 
-	public synchronized static void createNewPage(Object userID)
-	{
-
-	}
-
+	/*
+	 * Page and Section Load 
+	 */
 	public synchronized static Object loadPage(Object userID)
 	{
 		return null;
 	}
 
-	public synchronized static Object loadSection(Object userID, Object name)
+	public synchronized static Object loadSection(Object userID, Object sectionName)
 	{
 		return null;
 	}
 
-	public synchronized static void editProfilePic()
+	/*
+	 * Basic Profile Updates
+	 */
+	public synchronized static void editProfilePic(Object pic)
 	{
 
 	}
 
-	public synchronized static void editBasicInfo()
+	public synchronized static void editBasicInfo(Object info)
 	{
 
 	}
 
 	public synchronized static void editExperienceInfo()
 	{
-
+		throw new UnsupportedOperationException();
 	}
 
-	public synchronized static void AddToTrustList(Object id)
+	/*
+	 * Explicit Signal about Skill Set, Trusted Talents, Team Management
+	 */
+	public synchronized static void AddTrustedTalent(Object id)
 	{
 
 	}
 
-	public synchronized static void AddToSkillList(Object id)
+	public synchronized static void RemoveTrustedTalent(Object id)
 	{
 
 	}
 
+	public synchronized static void AddSkill(Object id)
+	{
+
+	}
+	
+	public synchronized static void RemoveSkill(Object id)
+	{
+
+	}
+
+	public synchronized static void ListContacts(Object id)
+	{
+
+	}
+	
+	public synchronized static void CreateTeam(Object id)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public synchronized static void DeleteTeam(Object id)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public synchronized static void AddToTeam(Object id)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public synchronized static void RemoveFromTeam(Object id)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	/*
+	 * Project Bidding and Ordering
+	 */
+	public synchronized static void BidOnProject(Object id)
+	{
+
+	}
+
+	public synchronized static void BidOnTeamProject(Object id)
+	{
+
+	}
+
+	public synchronized static void AcceptProject(Object id)
+	{
+
+	}
+
+	public synchronized static void PlaceProjectOrder(Object id)
+	{
+
+	}
+
+	/*
+	 * Invitation Feature to increase word-of-mouth advertising
+	 */
 	public synchronized static void InviteLinkedInContacts(Object fbID)
 	{
 
@@ -72,22 +136,26 @@ public class WorkAppUserProfilePage implements IPage
 
 	public synchronized static void InvitePhoneContacts(Object emailID)
 	{
-
+		throw new UnsupportedOperationException();
 	}
 
+	
+	/*
+	 * Sharing Feature to increase word-of-mouth advertising
+	 */
 	public synchronized static void ShareOnFB(Object fbID)
 	{
-
+		throw new UnsupportedOperationException();
 	}
 
 	public synchronized static void ShareOnTwitter(Object fbID)
 	{
-
+		throw new UnsupportedOperationException();
 	}
 
 	public synchronized static void ShareOnLinkedIn(Object fbID)
 	{
-
+		throw new UnsupportedOperationException();
 	}
 
 }
