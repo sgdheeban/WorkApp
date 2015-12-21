4f896ffb5cfacd8fc3df4964b714412df8b73cca
sgdheeban <sgdheeban@gmail.com>
2015-12-20 19:49:29 -0800

    orm example

--- WorkAppServer/src/test/java/com/workappinc/workappserver/dataaccess/resources/examples/WorkAppOrmExample.java
+++ WorkAppServer/src/test/java/com/workappinc/workappserver/dataaccess/resources/examples/WorkAppOrmExample.java
@@ -1,5 +1,7 @@
 package com.workappinc.workappserver.dataaccess.resources.examples;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -7,18 +9,29 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import com.workappinc.workappserver.common.logging.IApplicationLogger;
 import com.workappinc.workappserver.common.logging.WorkAppLogger;
+import com.workappinc.workappserver.dataaccess.orm.annotations.Table;
+import com.workappinc.workappserver.dataaccess.orm.core.Persist;
+import com.workappinc.workappserver.dataaccess.resources.examples.testtable.User;
 import com.workappinc.workappserver.dataaccess.resources.implementation.WorkAppJDBCConnection;
 import com.workappinc.workappserver.dataaccess.resources.implementation.WorkAppMySQLConnectionManager;
 
+/**
+ * Test ORM Example
+ * 
+ * @author dhgovindaraj
+ *
+ */
 public class WorkAppOrmExample
 {
+
 	public void simpleTableORMExample()
 	{
 		IApplicationLogger logger = WorkAppLogger.getInstance(null);
-		String dbUrl = "jdbc:mysql://localhost:3306/";
+		String dbUrl = "jdbc:mysql://localhost:3306/testdb";
 		String username = "root";
 		String password = "password";
 		WorkAppJDBCConnection conn = null;
@@ -35,7 +48,14 @@ public class WorkAppOrmExample
 			connections = (WorkAppMySQLConnectionManager) WorkAppMySQLConnectionManager.getInstance(dbUrl, username,
 					password, logger);
 			conn = connections.getConnection();
-
+			Persist persist = new Persist(conn);
+			List<User> users = persist.readList(User.class, "select * from user where age > ?", 900);
+			System.out.println("Size: " + users.size());
+			System.out.println("List of Retrieved Records:");
+			for (User x : users)
+			{
+				System.out.println(x.toString());
+			}
 		}
 		catch (SQLException ex)
 		{
@@ -59,7 +79,8 @@ public class WorkAppOrmExample
 
 	public static void main(String args[]) throws IOException
 	{
-
+		WorkAppOrmExample orm = new WorkAppOrmExample();
+		orm.simpleTableORMExample();
 	}
 
 }
