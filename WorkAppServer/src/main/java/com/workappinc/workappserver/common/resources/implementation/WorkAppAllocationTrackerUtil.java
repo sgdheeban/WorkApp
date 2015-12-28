d1404841a706d34a635e6dfd5b560efa473a568f
sgdheeban <sgdheeban@gmail.com>
2015-12-27 21:48:40 -0800

    WorkAppAllocationTracker in progress

---
+++ WorkAppServer/src/main/java/com/workappinc/workappserver/common/resources/implementation/WorkAppAllocationTrackerUtil.java
@@ -0,0 +1,22 @@
+package com.workappinc.workappserver.common.resources.implementation;
+
+/**
+ * WorkAppAllocationTrackerUtil is a util class which tracks all objects created
+ * by WorkApp
+ * 
+ * @author dhgovindaraj
+ *
+ */
+public class WorkAppAllocationTrackerUtil
+{
+	/**
+	 * Tracks all objects monitored under profiler Causes program to run slower,
+	 * so use it optionally Tracks all heap object allocations, and specified
+	 * constructor allocations as well
+	 * 
+	 */
+	public static void trackObjectAllocation()
+	{
+
+	}
+}
