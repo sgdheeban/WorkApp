66cf50206ec4baaf59648a68bc986115e86ec994
sgdheeban <sgdheeban@gmail.com>
2016-01-06 17:51:43 -0800

    main method work in progress

--- WorkAppServer/src/main/java/com/workappinc/workappserver/businesslogic/main/WorkAppMainServer.java
+++ WorkAppServer/src/main/java/com/workappinc/workappserver/businesslogic/main/WorkAppMainServer.java
@@ -1,8 +1,10 @@
 package com.workappinc.workappserver.businesslogic.main;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.workappinc.workappserver.dataaccess.resources.implementation.WorkAppArgument;
+import com.workappinc.workappserver.dataaccess.resources.implementation.WorkAppCommandLineArgsReader;
 
 /**
  * 
@@ -13,22 +15,22 @@ import com.workappinc.workappserver.dataaccess.resources.implementation.WorkAppA
  */
 public class WorkAppMainServer
 {
-	
-	@WorkAppArgument(alias = "c", description = "Config File Location", required = true)
+
+	@WorkAppArgument(alias = "c", description = "Config File Location (Required)", required = true)
 	private static String configFile;
 
-	@WorkAppArgument(alias = "db", description = "DB Config File Location", required = true)
+	@WorkAppArgument(alias = "db", description = "DB Config File Location")
 	private static String dbConfigFile;
-	
-	@WorkAppArgument(alias = "m", description = "Start Mode - Choose between Rest, Thrift, Batch")
+
+	@WorkAppArgument(alias = "m", description = "Start Mode - Choose between rest, thrift, batch")
 	private static String mode;
-	
+
 	@WorkAppArgument(alias = "p", description = "Start Port")
 	private static Integer port = 8080;
 
 	@WorkAppArgument(alias = "s", description = "DB Schema File Location")
 	private static String schemaFile;
-	
+
 	@WorkAppArgument(alias = "l", description = "Log Level")
 	private static String logLevel;
 
@@ -57,7 +59,7 @@ public class WorkAppMainServer
 	 */
 	public static void main(String args[]) throws IOException
 	{
-		// Step1: Read Command-Line Options into Properties
+
 		// Step2 : Read config file and update Properties
 		// Step3: Read Log config file, if any and get a Logger Instance
 		// Step4: Instantiate JSON Parser for reading any JSON Config
@@ -66,8 +68,21 @@ public class WorkAppMainServer
 		// Step7: Start an Jetty-HTTP or Thrift server to serve requests
 
 		System.out.println("Hello World ! ");
-		
-		
+
+		// Step1: Read Command-Line Options into Properties
+		@SuppressWarnings("unused")
+		final List<String> parse;
+		try
+		{
+			parse = WorkAppCommandLineArgsReader.parse(WorkAppMainServer.class, args);
+		}
+		catch (IllegalArgumentException e)
+		{
+			WorkAppCommandLineArgsReader.usage(WorkAppMainServer.class);
+			System.exit(1);
+			return;
+		}
+
 	}
 
 }
