741a81c9db74da18056753dc4c7e9a55842c38fa
sgdheeban <sgdheeban@gmail.com>
2016-01-24 22:18:29 -0800

    refactoring logger in progress

--- WorkAppServer/src/main/java/com/workappinc/workappserver/common/logging/WorkAppLogger.java
+++ WorkAppServer/src/main/java/com/workappinc/workappserver/common/logging/WorkAppLogger.java
@@ -24,8 +24,7 @@ public class WorkAppLogger implements IApplicationLogger
 {
 	private ConcurrentHashMap<Class<?>, Logger> mLoggerInstances = new ConcurrentHashMap<Class<?>, Logger>();
 	private static IApplicationLogger mInstance = null;
-	private StringBuilder mStringBuilder = null;
-
+	
 	private WorkAppLogger(Properties config) throws SystemException
 	{
 		// If External Config File Exists, override default log4j.properties
@@ -74,7 +73,7 @@ public class WorkAppLogger implements IApplicationLogger
 		PropertyConfigurator.configure(config);
 	}
 
-	private synchronized Logger getLoggerInstance(Class<?> className)
+	private Logger getLoggerInstance(Class<?> className)
 	{
 		Logger returnLogger = null;
 		returnLogger = mLoggerInstances.putIfAbsent(className, Logger.getLogger(className));
@@ -83,59 +82,60 @@ public class WorkAppLogger implements IApplicationLogger
 	}
 
 	@Override
-	public synchronized void LogPerf(Object ctx, Class<?> className)
+	public void LogPerf(Object ctx, Class<?> className)
 	{
 		if (getLoggerInstance(className).isInfoEnabled())
 			getLoggerInstance(className).info(format(ctx));
 	}
 
 	@Override
-	public synchronized void LogException(Object ctx, Class<?> className)
+	public void LogException(Object ctx, Class<?> className)
 	{
 		if (getLoggerInstance(className).isInfoEnabled())
 			getLoggerInstance(className).info(format(ctx));
 	}
 
 	@Override
-	public synchronized void LogFatal(Object ctx, Class<?> className)
+	public void LogFatal(Object ctx, Class<?> className)
 	{
 		getLoggerInstance(className).fatal(format(ctx));
 	}
 
 	@Override
-	public synchronized void LogError(Object ctx, Class<?> className)
+	public void LogError(Object ctx, Class<?> className)
 	{
 		getLoggerInstance(className).error(format(ctx));
 	}
 
 	@Override
-	public synchronized void LogWarn(Object ctx, Class<?> className)
+	public void LogWarn(Object ctx, Class<?> className)
 	{
 		getLoggerInstance(className).warn(format(ctx));
 	}
 
 	@Override
-	public synchronized void LogInfo(Object ctx, Class<?> className)
+	public void LogInfo(Object ctx, Class<?> className)
 	{
 		if (getLoggerInstance(className).isInfoEnabled())
 			getLoggerInstance(className).info(format(ctx));
 	}
 
 	@Override
-	public synchronized void LogDebug(Object ctx, Class<?> className)
+	public void LogDebug(Object ctx, Class<?> className)
 	{
 		if (getLoggerInstance(className).isDebugEnabled())
 			getLoggerInstance(className).debug(format(ctx));
 	}
 
 	@Override
-	public synchronized void LogTrace(Object ctx, Class<?> className)
+	public void LogTrace(Object ctx, Class<?> className)
 	{
 		getLoggerInstance(className).trace(format(ctx));
 	}
 
 	private String format(Object ctx)
 	{
+		StringBuilder mStringBuilder = null;
 		String returnString = null;
 		if (ctx instanceof Exception)
 		{
