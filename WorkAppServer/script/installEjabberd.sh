c04f325d862366d1e922b6900e1219654d291005
sgdheeban <sgdheeban@gmail.com>
2016-02-24 17:05:50 -0800

    adding scripts

---
+++ WorkAppServer/script/installEjabberd.sh
@@ -0,0 +1,3 @@
+apt-get install erlang
+apt-get install ejabberd
+ejabberdctl register admin localhost password
---
+++ WorkAppServer/script/manageServices.sh
@@ -0,0 +1,5 @@
+echo "Managing MySQL"
+sudo service mysql $1
+
+echo "Managing eJabberd"
+sudo service ejabberd $1
