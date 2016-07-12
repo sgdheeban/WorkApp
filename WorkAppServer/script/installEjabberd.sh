ebbe81ef5141cb79863f40660c9a96c69c13e30c
sgdheeban <sgdheeban@gmail.com>
2016-07-12 00:33:01 -0700

    refactor

--- WorkAppServer/script/installEjabberd.sh
+++
@@ -1,3 +0,0 @@
-apt-get install erlang
-apt-get install ejabberd
-ejabberdctl register admin localhost password
--- WorkAppServer/script/manageServices.sh
+++ WorkAppServer/script/manageServices.sh
@@ -1,5 +1,2 @@
 echo "Managing MySQL"
 sudo service mysql $1
-
-echo "Managing eJabberd"
-sudo service ejabberd $1
